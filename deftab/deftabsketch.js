18d5fea52fe90331eceedfb126c9759a99784abf
SunSharks <lysanne.rogers@gmail.com>
2022-05-01 05:30:20 +0200

    endlich wählbare griditems

--- deftab/deftabsketch.js
+++ deftab/deftabsketch.js
@@ -1,3 +1,4 @@
+
 let num_days = 3;
 let num_jobs = 8;
 let headerheight = 20;
@@ -9,6 +10,8 @@ let row_height;
 let ww;
 let wh;
 
+let griditems = [];
+let num_griditems = 0;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -16,109 +19,180 @@ function setup() {
   row_height = (windowHeight - headerheight) / num_jobs;
   ww = windowWidth;
   wh = windowHeight;
+  grid = new Grid();
+
 }
 
+
+// function mousePressed() {
+//   // inputcolorbutton.collides();
+//
+//   if (mouseButton === RIGHT) {
+//     // Get the same color as a right-clicked circle.
+//         for(var i=0; i<circles.length; i++){
+//           if (circles[i].collides()){
+//             colormode = i;
+//             c[0] = circles[i].curr_color[0];
+//             c[1] = circles[i].curr_color[1];
+//             c[2] = circles[i].curr_color[2];
+//             for (var s=0; s<sliders.length; s++){
+//               sliders[s].set_pos(c[s]);
+//             }
+//           }
+//       }
+//       return;
+//     }
+//     if(mouseX <= tri_area_dim){
+//       mode = 1;
+//     }
+//     else{
+//       mode = 0;
+//     }
+//   if (mode == 0){
+//     colormode = -1
+//     for(let i = 0; i < sliders.length; i++) {
+//       sliders[i].press();
+//     }
+//   }
+//   else if (mode == 1){
+//     console.log("in mode == 1");
+//     for (var i=0; i<circles.length; i++){
+//       if (circles[i].collides()){
+//         circles[i].update();
+//       }
+//     }
+//   }
+//   colormode = -1
+// }
 
 function mousePressed() {
-  // inputcolorbutton.collides();
-
-  if (mouseButton === RIGHT) {
-    // Get the same color as a right-clicked circle.
-        for(var i=0; i<circles.length; i++){
-          if (circles[i].collides()){
-            colormode = i;
-            c[0] = circles[i].curr_color[0];
-            c[1] = circles[i].curr_color[1];
-            c[2] = circles[i].curr_color[2];
-            for (var s=0; s<sliders.length; s++){
-              sliders[s].set_pos(c[s]);
-            }
-          }
-      }
-      return;
-    }
-    if(mouseX <= tri_area_dim){
-      mode = 1;
-    }
-    else{
-      mode = 0;
-    }
-  if (mode == 0){
-    colormode = -1
-    for(let i = 0; i < sliders.length; i++) {
-      sliders[i].press();
-    }
-  }
-  else if (mode == 1){
-    console.log("in mode == 1");
-    for (var i=0; i<circles.length; i++){
-      if (circles[i].collides()){
-        circles[i].update();
-      }
-    }
-  }
-  colormode = -1
-}
-
-function mousePressed() {
-  for (let i = 0; i < bubbles.length; i++) {
-    bubbles[i].clicked(mouseX, mouseY);
+  for (let i = 0; i < num_jobs * num_days; i++) {
+    griditems[i].clicked(mouseX, mouseY);
   }
 }
 
-
-function create_grid(){
-  let x = 0;
-  let y = headerheight;
-  for(let i=0; i<num_jobs; i++){
-    line(x, y, ww, y)
-    y += row_height;
-  }
-  x = rowheaderwidth;
-  y = 0;
-  for(let i=0; i<num_days; i++){
-    line(x, 0, x, wh)
-    x += col_width;
-  }
-
-}
+// function mousePressed() {
+//   // inputcolorbutton.collides();
+//
+//   if (mouseButton === RIGHT) {
+//     // Get the same color as a right-clicked circle.
+//         for(var i=0; i<circles.length; i++){
+//           if (circles[i].collides()){
+//             colormode = i;
+//             c[0] = circles[i].curr_color[0];
+//             c[1] = circles[i].curr_color[1];
+//             c[2] = circles[i].curr_color[2];
+//             for (var s=0; s<sliders.length; s++){
+//               sliders[s].set_pos(c[s]);
+//             }
+//           }
+//       }
+//       return;
+//     }
+//     if(mouseX <= tri_area_dim){
+//       mode = 1;
+//     }
+//     else{
+//       mode = 0;
+//     }
+//   if (mode == 0){
+//     colormode = -1
+//     for(let i = 0; i < sliders.length; i++) {
+//       sliders[i].press();
+//     }
+//   }
+//   else if (mode == 1){
+//     console.log("in mode == 1");
+//     for (var i=0; i<circles.length; i++){
+//       if (circles[i].collides()){
+//         circles[i].update();
+//       }
+//     }
+//   }
+//   colormode = -1
+// }
 
 function draw() {
-  background(255);
+  // background(255);
   // print(col_width);
-  create_grid();
+  // create_grid();
 }
 
 
+
+
+class Grid {
+  constructor(){
+    this.rows = [];
+    this.cols = [];
+    this.assemble_grid();
+  }
+
+  make_data_row(y, num_cols=num_days){
+    let x = rowheaderwidth;
+    let l = [];
+    for (let i=0; i<num_cols; i++){
+      let r = new Griditem(num_griditems, x, y);
+      num_griditems += 1;
+      r.show();
+      griditems.push(r);
+      l.push(r);
+      x += col_width;
+    }
+    this.rows.push(l);
+  }
+
+  assemble_grid(num_rows=num_jobs, num_cols=num_days){
+    let y = headerheight;
+    for (let i=0; i<num_rows; i++){
+      this.make_data_row(y);
+      y += row_height;
+    }
+    let col = [];
+    for (let i=0; i<num_cols; i++){
+
+      for (let j=0; j<num_rows; j++){
+        col.push(this.rows[j][i])
+      }
+      this.cols.push(col)
+      col = [];
+    }
+  }
+
+
+}
+
 class Griditem {
-  constructor(x, y, w=col_width, h=row_height) {
+  constructor(id, x, y, w=col_width, h=row_height) {
+    this.id = id
     this.x = x; // upper left corner
     this.y = y; // upper left corner
     this.w = w;
     this.h = h;
     this.content = "";
+    this.color = "red";
   }
 
-  isin(x, y) {
+  collides(x, y) {
     let ret = x > this.x && x < this.x + this.w && y > this.y && y < this.y + this.h
     return ret
   }
 
   clicked(x, y) {
-    if (isin(x, y)) {
-      this.color = 255;
+    if (this.collides(x, y)) {
+      this.color = "green";
+      this.show();
+      print("clicked." + this.id.toString());
     }
-  }
-
-  move() {
-    this.x = this.x + random(-2, 2);
-    this.y = this.y + random(-2, 2);
   }
 
   show() {
     stroke(255);
     strokeWeight(4);
-    fill(this.brightness, 125);
-    ellipse(this.x, this.y, this.r * 2);
+    fill(255);
+    rect(this.x, this.y, this.w, this.h);
+    fill(this.color);
+    rect(this.x, this.y, this.w, this.h);
+    print("draw " + this.id.toString())
   }
 }

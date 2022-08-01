8ccffb83733ca92b90e5551e3983faef0b727a3e
SunSharks <lysanne.rogers@gmail.com>
2022-08-01 03:43:25 +0200

    biases included in stats.

--- crew_prios/stats.php
+++ crew_prios/stats.php
@@ -2,7 +2,7 @@
 // include("db.php");
 $jobs_count = fetch_it("SELECT count(id) FROM Jobs WHERE Jobs.jt_primary IN (SELECT id FROM Jobtypes WHERE helper=0);")[0]["count(id)"];
 $work_hours_sum = fetch_it("SELECT sum(during) FROM Jobs WHERE Jobs.jt_primary IN (SELECT id FROM Jobtypes WHERE helper=0);")[0]["sum(during)"];
-$bias_work_hours = fetch_it("SELECT sum(bias) FROM Names;")[0]["sum(bias)"];
+$bias_work_hours = fetch_it("SELECT sum(bias) FROM Users;")[0]["sum(bias)"];
 $num_persons = fetch_it("SELECT count(id) FROM Names WHERE Names.helper = 0;")[0]["count(id)"];
 $work_hours_avg = ($work_hours_sum + $bias_work_hours) / $num_persons;
 $num_registered_users = fetch_it("SELECT count(id) FROM Users;")[0]["count(id)"];

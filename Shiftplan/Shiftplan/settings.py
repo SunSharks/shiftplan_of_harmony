bdc2357888fe17a64f36db9c5a6e2b743c0781a4
SunSharks <lysanne.rogers@gmail.com>
2022-11-11 02:45:23 +0100

    ignore

--- Shiftplan/Shiftplan/settings.py
+++ Shiftplan/Shiftplan/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+from decouple import config
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=%%s=ljy4(u=#(fb2!cq+=kk5y8th=jyf(7^syv2i4=%n^1vp9'
+
+SECRET_KEY = config("SECRET_KEY")  # this is to replace the secret key you cut away before
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

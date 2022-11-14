9c5dc99353d44106e831a6e6cfbf69d381872010
SunSharks <lysanne.rogers@gmail.com>
2022-11-15 00:31:34 +0100

    add forms.py, working jobtype form

---
+++ Shiftplan/defs/forms.py
@@ -0,0 +1,44 @@
+from django import forms
+from django.forms.models import inlineformset_factory
+
+from .models import Shiftplan, Jobtype, Job, TimeInterval
+
+
+# class RenewShiftplanForm(forms.Form):
+#     new_name = forms.CharField(help_text="Enter a new name.")
+
+class TimeIntervalForm(forms.ModelForm):
+    class Meta:
+        model = TimeInterval
+        fields = (
+            'start_date',
+            'end_date'
+        )
+
+
+class JobtypeForm(forms.ModelForm):
+    class Meta:
+        model = Jobtype
+        fields = (
+            'name',
+            'description'
+        )
+
+
+TimeFormSet = inlineformset_factory(
+    Shiftplan,
+    TimeInterval,
+    form=TimeIntervalForm,
+    min_num=0,  # minimum number of forms that must be filled in
+    extra=1,  # number of empty forms to display
+    can_delete=True  # show a checkbox in each form to delete the row
+)
+
+JobtypeFormSet = inlineformset_factory(
+    Shiftplan,
+    Jobtype,
+    form=JobtypeForm,
+    min_num=0,  # minimum number of forms that must be filled in
+    extra=1,  # number of empty forms to display
+    can_delete=True  # show a checkbox in each form to delete the row
+)

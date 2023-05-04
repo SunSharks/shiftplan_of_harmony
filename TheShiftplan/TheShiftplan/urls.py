5888a490d57fa691d1ca060ff06ae0377a27757b
SunSharks <lysanne.rogers@gmail.com>
2023-05-04 23:56:59 +0200

    update home.html

--- TheShiftplan/TheShiftplan/urls.py
+++ TheShiftplan/TheShiftplan/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include
 from django.views.generic.base import TemplateView
 
 urlpatterns = [
+    path('', TemplateView.as_view(template_name='home.html'), name='home'),
     path("accounts/", include("django.contrib.auth.urls")),
     path('djaploda/', include('django_plotly_dash.urls')),
     path('admin/', admin.site.urls),

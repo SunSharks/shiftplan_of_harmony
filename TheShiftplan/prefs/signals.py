84f6eabdec57ef4752b2a63ced24958c96ee4d1f
SunSharks <lysanne.rogers@gmail.com>
2023-05-07 00:23:20 +0200

    Signals: Add UserJobRatings for every job when user is created and for every user if new job is created.

--- TheShiftplan/prefs/signals.py
+++ TheShiftplan/prefs/signals.py
@@ -1,11 +1,26 @@
-from .models import BiasHours, UserOptions
+from .models import BiasHours, UserOptions, UserJobRating
+from defs.models import Job
 
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 @receiver(post_save, sender=User)
-def create_user_models(sender, instance, created, **kwargs):
+def user_created(sender, instance, created, **kwargs):
     if created:
         BiasHours.objects.create(user=instance)
         UserOptions.objects.create(user=instance)
+
+        jobs = Job.objects.all()
+        for j in jobs:
+            UserJobRating.objects.create(user=instance, job=j, rating=j.rating)
+
+
+@receiver(post_save, sender=Job)
+def job_created(sender, instance, created, **kwargs):
+    if created:
+        users = User.objects.all()
+        for u in users:
+            UserJobRating.objects.create(user=u, job=instance, rating=instance.rating)
+
+

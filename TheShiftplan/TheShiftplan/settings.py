3a3923d57e29b050fce5a7bf115e72c806e73140
yoshi253 <haerichj@gmail.com>
2023-05-09 21:19:20 +0200

    Add bootstrap5 scss and compressor

--- TheShiftplan/TheShiftplan/settings.py
+++ TheShiftplan/TheShiftplan/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'nested_admin',
     'defs',
     'prefs',
+    'compressor',
     'bootstrap4',
 ]
 
@@ -125,6 +126,20 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
+STATICFILES_FINDERS = [
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'compressor.finders.CompressorFinder',
+]
+
+COMPRESS_PRECOMPILERS = (
+    ('text/x-scss', 'django_libsass.SassCompiler'),
+)
+COMPRESS_ROOT = BASE_DIR / "static"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field

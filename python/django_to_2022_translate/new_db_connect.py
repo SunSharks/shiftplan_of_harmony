9902e17fda857208686cfa9b04c20f3823dcce52
SunSharks <lysanne.rogers@gmail.com>
2023-05-11 19:23:42 +0200

    removed old sql functions.

--- python/django_to_2022_translate/new_db_connect.py
+++ python/django_to_2022_translate/new_db_connect.py
@@ -5,10 +5,8 @@
 # import mysql.connector
 import pandas as pd
 import json
-
-from get_db_json import models_names
-
-from config import shiftplans
+from os.path import join
+from get_db_json import models_names, paths
 
 
 
@@ -18,207 +16,26 @@ shiftplan_name = "Crew"
 def build_tables_structure():
     models = {}
     for fn in models_names:
-        with open(fn+".json", 'r') as f:
+        with open(join(paths["json_out"], fn+".json"), 'r') as f:
             models.update({fn: pd.json_normalize(json.loads(f.read()))})
     # print(models)
+    for m in models:
+        models[m] = models[m].rename(columns={old: old.replace("fields.", "", 1).lstrip() for old in models[m].columns})
     return models
 # df = df.drop(df[<some boolean condition>].index)
 
-def fetch_all(sql, result_struct="dict"):
-    # cnx = mysql.connector.connect(**config)
-    # if result_struct == "dict":
-    #     cur = cnx.cursor(dictionary=True)
-    # else:
-    #     cur = cnx.cursor()
-    # cur.execute(sql)
+def make_jobs():
+    pass
 
-    # # Fetch one result
-    # row = cur.fetchall()
-    # cnx.close()
-    row = None
-    return row
-
-
-def fetch_one(sql, result_struct="dict"):
-    # cnx = mysql.connector.connect(**config)
-    # if result_struct == "dict":
-    #     cur = cnx.cursor(dictionary=True)
-    # else:
-    #     cur = cnx.cursor()
-    # cur.execute(sql)
-    # # Fetch one result
-    # row = cur.fetchone()
-    # cnx.close()
-    row = None
-    return row
-
-
-def fetch_days():
-    sql = """ SELECT id,
-    name,
-    date
-    FROM Days"""
-    days = {}
-    return fetch_all(sql)
-
-
-def fetch_jobtypes(shiftplan_pk):
-    jts = models["jobtypes"].loc[
-            models["jobtypes"]["fields.shiftplan"] == shiftplan_pk]
-    # print(jts)
-    sql = """ SELECT id,
-    name,
-    competences,
-    special,
-    helper,
-    name_appendix
-    FROM Jobtypes"""
-    jts = jts.loc[jts["fields.shiftplan"] == group]
-    for c in jts.columns:
-        print(c)
-    return jts
-
-
-def fetch_jobs():
-    sql = """ SELECT id,
-    abs_start,
-    abs_end,
-    during,
-    start_day_id,
-    end_day_id,
-    dt_start,
-    dt_end,
-    jt_primary
-    FROM Jobs"""
-    return fetch_all(sql)
-
-
-def fetch_jobs_by_group(helper):
-    sql = """ SELECT id,
-    abs_start,
-    abs_end,
-    during,
-    start_day_id,
-    end_day_id,
-    dt_start,
-    dt_end,
-    jt_primary
-    FROM Jobs WHERE jt_primary IN(
-    SELECT id from Jobtypes WHERE helper = {}
-    )""".format(int(helper))
-    return fetch_all(sql)
-
-
-def fetch_names(helper=False):
-    sql = """ SELECT id,
-    surname,
-    famname,
-    registered
-    FROM Names"""
-    if not helper == "all":
-        sql += """ WHERE helper = {}""".format(int(helper))
-    return fetch_all(sql)
-
-
-def fetch_users():
-    sql = """ SELECT id,
-    fullname_id,
-    nickname,
-    email,
-    break,
-    bias
-    FROM Users"""
-    return fetch_all(sql)
-
-
-def fetch_helpers():
-    sql = """ SELECT id,
-    fullname_id,
-    nickname,
-    email,
-    break,
-    workload
-    FROM Helpers"""
-    return fetch_all(sql)
-
-
-def fetch_preferences(name_id):
-    jobnames = fetch_column_names("Preferences", addon=" AND  COLUMN_NAME LIKE 'job%'")
-    sql = "SELECT "
-    for jn in jobnames:
-        # print(jn[0])
-        sql += jn[0] + ", "
-    sql = sql.rstrip()[:-1]
-    sql += " FROM Preferences WHERE name_id = {}".format(name_id)
-    # print(sql)
-    return fetch_one(sql)
-
-
-def fetch_job_ids_by_group(helper):
-    sql = """ SELECT id
-    FROM Jobs WHERE jt_primary IN(
-    SELECT id from Jobtypes WHERE helper = {}
-    )""".format(int(helper))
-    return fetch_all(sql)
-
-
-def fetch_preferences_by_group(group):
-    if group == "user":
-        group_job_ids = fetch_job_ids_by_group(False)
-    elif group == "helper":
-        group_job_ids = fetch_job_ids_by_group(True)
-    colnamesstr = ""
-    for d in group_job_ids:
-        colnamesstr += "job{},".format(d["id"])
-    colnamesstr = colnamesstr[:-1]
-    jobnames = fetch_column_names("Preferences", addon="")
-    sql = "SELECT name_id, {colnames} FROM Preferences WHERE name_id IN ".format(
-        colnames=colnamesstr)
-    if group == "user":
-        sql += "(SELECT id FROM Names WHERE Names.helper=0);"
-    elif group == "helper":
-        sql += "(SELECT id FROM Names WHERE Names.helper=1);"
-    # print(sql)
-    return fetch_all(sql)
-
-
-def fetch_all_preferences():
-    jobnames = fetch_column_names("Preferences", addon="")
-    sql = "SELECT "
-    for jn in jobnames:
-        sql += jn + ", "
-    # assert 1==0
-    sql = sql.rstrip()[:-1]
-    sql += " FROM Preferences"
-    # print(sql)
-    return fetch_all(sql)
-
-
-def fetch_exclusives():
-    sql = "SELECT jt_name, fullname_id FROM Exclusives;"
-    # print(sql)
-    return fetch_all(sql)
-
-
-def fetch_column_names(tablename, addon=""):
-    sql = """SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS
-    WHERE TABLE_NAME= '{}' {}""".format(tablename, addon)
-    ret = fetch_all(sql, "lst")
-    # print([i[0] for i in ret])
-    return [i[0] for i in ret]
 
 
 if __name__ == "__main__":
-    # print(fetch_all("SELECT * FROM Jobtypes"))
-    # print(50*"_")
-    # print(fetch_preferences(9))
-    # print(fetch_all("SELECT id FROM Names WHERE registered = 1"))
     models = build_tables_structure()
-    # fetch_jobtypes()
-    for sp in shiftplans:
-        # print(models["shiftplans"].loc[models["shiftplans"]["fields.name"] == sp])
-        shiftplan = models["shiftplans"].loc[models["shiftplans"]["fields.name"] == sp]
-        jobtypes = models["jobtypes"].loc[
-            models["jobtypes"]["fields.shiftplan"] == shiftplan["pk"][0]]
-        # jobtypes = fetch_jobtypes(shiftplan)
-        print(jobtypes)
+    jobtypes = models["jobtypes"]
+    print(jobtypes)
+    models["jobs"]["begin"] = pd.to_datetime(models["jobs"]['begin_date'] + ' ' + models["jobs"]['begin_time'])
+    models["jobs"]["end"] = pd.to_datetime(models["jobs"]['end_date'] + ' ' + models["jobs"]['end_time'])
+    models["jobs"]["during"] = models["jobs"]["end"] - models["jobs"]["begin"]
+    print(models["user_profiles"])
+    workers = models["user_profiles"].loc[models["user_profiles"]["worker"] == True]
+    print(workers) 

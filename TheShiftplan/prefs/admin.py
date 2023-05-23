dc3e504d362ba1c5d83d1a7d7be7c1ff2757de9f
SunSharks <lysanne.rogers@gmail.com>
2023-05-23 23:44:43 +0200

    Add filters to UserJobRating admin.

--- TheShiftplan/prefs/admin.py
+++ TheShiftplan/prefs/admin.py
@@ -3,10 +3,20 @@ from django.conf import settings
 
 from .signals import *
 
-from .models import BiasHours
+from .models import BiasHours#, Workload
 admin.site.register(BiasHours)
+# admin.site.register(Workload)
+
 
 if settings.DEBUG:
     from .models import UserOptions, UserJobRating
     admin.site.register(UserOptions)
-    admin.site.register(UserJobRating)
+
+    class UserJobRatingAdmin(admin.ModelAdmin):
+        model = UserJobRating
+        # That adds a “Filter” sidebar that lets people filter the change list by field:
+        list_filter = ['user', 'job', 'rating']
+        # That adds a search box at the top of the change list.
+        search_fields = ['user', 'job', 'rating']
+
+    admin.site.register(UserJobRating, UserJobRatingAdmin)

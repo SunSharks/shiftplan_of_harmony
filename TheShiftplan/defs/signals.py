3120287a02f3aec975f36c4909f5387abafaa469
SunSharks <lysanne.rogers@gmail.com>
2023-07-20 00:18:49 +0200

    Strange newline.

--- TheShiftplan/defs/signals.py
+++ TheShiftplan/defs/signals.py
@@ -2,9 +2,10 @@ from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
-from .models import UserProfile
+from .models import UserProfile, Jobtype, Job
 
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
         UserProfile.objects.create(user=instance)
+        
